de607b9c67098b69b100b654a18bd9cb4790e17a
Shishikin <mvg1313@mail.ru>
2023-07-09 15:40:17 +0300

    Thread

--- CheshSumma/MainWindow.xaml.cs
+++ CheshSumma/MainWindow.xaml.cs
@@ -15,12 +15,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+
 
 namespace CheshSumma
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -30,12 +33,28 @@ namespace CheshSumma
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            void Hashsumma()
+            {
+                var dialog = new OpenFileDialog();
+                if (dialog.ShowDialog() == true)
+                {
+                    var md5 = MD5.Create().ComputeHash(File.ReadAllBytes(dialog.FileName));
+                    MessageBox.Show(BitConverter.ToString(md5));
+                }
+            }
+            ThreadStart threadStart = new ThreadStart(Hashsumma);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            Hashsumma();
+
+ /*           
             var dialog = new OpenFileDialog();
             if (dialog.ShowDialog() == true)
             {
                 var md5 = MD5.Create().ComputeHash(File.ReadAllBytes(dialog.FileName));
                 MessageBox.Show(BitConverter.ToString(md5));
             }
+ */
         }
     }
 }
